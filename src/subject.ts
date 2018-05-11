94b775a02a17015febf02a9850dc5e67cc5a1004
Ondra Machala <ondrej.machala@monkeydata.com>
2018-05-11 10:58:28 +0200

    Update package.json and docs. Add interfaces into subject.ts

--- src/subject.ts
+++ src/subject.ts
@@ -1,6 +1,26 @@
 'use strict';
 
-import {IListener, ISubject, ISubscriber} from "./interfaces";
+export interface ISubscriber {
+    unsubscribe();
+}
+
+export interface IListener {
+    _id: any;
+
+    callback(data?: any);
+}
+
+export interface ISubject {
+    getLast(): any;
+
+    next(data?: any): ISubject;
+
+    nextUnique(data?: any): ISubject;
+
+    subscribe(callback: Function): ISubscriber;
+
+    replayAndSubscribe(callback: Function): ISubscriber;
+}
 
 export class Subject implements ISubject {
 
